a4ba7b4f98facfc152296d3dd1245e6ded9759fe
jlperla <jesseperla@gmail.com>
2018-01-04 16:28:08 -0800

    Added another weave test

--- weave_sandbox/create_notebook.jl
+++ weave_sandbox/create_notebook.jl
@@ -2,5 +2,6 @@
 # Pkg.add("Weave")
 using Weave
 
-convert_doc("test.jmd", "test.ipynb") #This should generate the notebook
 #Right now (with JuliaPro 0.6.1) the notebook is not working when generated, but you can change the kernel to 0.6.1 and it seems to.
+convert_doc("test.jmd", "test.ipynb") #This should generate the notebook
+convert_doc("test_2.jl", "test_2.ipynb") #Note the .jl file has special comments in it.
---
+++ weave_sandbox/test_2.jl
@@ -0,0 +1,12 @@
+#' ---
+#' title : Testing weave with a .jl file
+#' author : UBC
+#' date : 1/4/2018
+#' ---
+
+#' # A Test of Weave
+a = 2 + 3
+
+#' # Display math?
+#' $a \equiv 2 + 3$
+
