0451b5f78dfba768bf8809b8e59410d236dbe361
Arnav Sood <soodarnav01@gmail.com>
2018-06-25 13:52:38 -0700

    Literate.jl testing

--- julia_sandbox/generate_docs.jl
+++ julia_sandbox/generate_docs.jl
@@ -1,5 +1,7 @@
 using Literate
 
+import Plots
+
 function update_date(content)
     content = replace(content, "DATETIME" => Date(now()))
     return content
@@ -7,4 +9,4 @@ end
 
 Literate.markdown(pwd() * "\\julia_sandbox\\literate_test.jl", pwd() * "\\julia_sandbox\\"; preprocess = update_date)
 Literate.notebook(pwd() * "\\julia_sandbox\\literate_test.jl", pwd() * "\\julia_sandbox\\"; preprocess = update_date)
-
+Literate.notebook(pwd() * "\\julia_sandbox\\literate_test_2.jl", pwd() * "\\julia_sandbox\\"; preprocess = update_date)
